2deef11e9c1387d5cf9d6a95df0a9e189baecd5b
takurinton <takurinton@takurinton.dev>
2024-04-21 11:50:17 +0900

    refactor (#5)

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    exclude: ["**/fixtures/**/*", "dist", "node_modules"],
+    exclude: ["dist", "node_modules"],
   },
 });
